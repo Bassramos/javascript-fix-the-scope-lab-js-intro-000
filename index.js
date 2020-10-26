c648567fe8314867c75d5fbabed0e637562f2593
Joshua Ramos <bassramos@gmail.com>
2020-10-26 22:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var animal = 'dog'
 
 function myAnimal() {
-  animal = cat;// You should not need to modify this function
+  var animal = cat;// You should not need to modify this function
   return animal
 }
 
